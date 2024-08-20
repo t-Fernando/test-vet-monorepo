36ff20333a88087710a9ff29aaa1087d68daa06c
t-Fernando <fegobo94@gmail.com>
2024-08-20 11:43:16 -0600

    monorepo with schema and t3

---
+++ apps/clinic/drizzle/0001_certain_paper_doll.sql
@@ -0,0 +1,1 @@
+DROP TABLE `clinic_post`;
---
+++ apps/clinic/drizzle/0002_neat_krista_starr.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `category` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`name` text
+);
+--> statement-breakpoint
+DROP TABLE `user`;
